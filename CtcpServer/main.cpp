aa2c60c2b4749774453a2cc123a2371ba13eaeb8
D0112 <1677528286@qq.com>
2024-05-26 12:03:51 +0800

    增加释放资源的代码

--- CtcpServer/main.cpp
+++ CtcpServer/main.cpp
@@ -2,17 +2,26 @@
 #include <signal.h>
 using namespace std;
 
+void FATHEXIT(int sig);
+void CHLDEXIT(int sig);
+
+CtcpServe ctcpserve;
+
 int main(int argc, char *argv[])
 
-{ // 避免出现僵尸进程
-    signal(SIGCHLD, SIG_IGN);
+{ // 避免出现僵尸进程，并关闭全部信号
+    for (int ii = 0; ii < 100; ++ii)
+        signal(ii, SIG_IGN);
+    if (fork() > 0)
+        exit(0);
+    signal(SIGINT, FATHEXIT);
+    signal(SIGTERM, FATHEXIT);
     if (argc != 2)
     {
         cout << "服务端端口号5005" << endl;
         return -1;
     }
 
-    CtcpServe ctcpserve;
     if (ctcpserve.initserve(atoi(argv[1])) == false)
     {
         perror("initserve()");
@@ -32,6 +41,8 @@ int main(int argc, char *argv[])
             ctcpserve.clientclose();
             continue;
         }
+        signal(SIGINT, CHLDEXIT);
+        signal(SIGTERM, CHLDEXIT);
         ctcpserve.listenclose();
         cout << "客户端已连接" << endl;
         string buffer;
@@ -52,4 +63,33 @@ int main(int argc, char *argv[])
             cout << "发送：" << buffer << endl;
         }
     }
+}
+
+void FATHEXIT(int sig)
+{
+    if (sig > 0)
+    {
+        signal(sig, SIG_IGN);
+        signal(SIGINT, SIG_IGN);
+        signal(SIGTERM, SIG_IGN);
+        cout << "catching the signal" << sig << endl;
+    }
+    kill(0, 15);
+    cout << "父进程退出" << endl;
+    // 编写善后代码（释放资源，提交或回滚事物）
+    exit(0);
+}
+
+void CHLDEXIT(int sig)
+{
+    if (sig > 0)
+    {
+        signal(sig, SIG_IGN);
+        signal(SIGINT, SIG_IGN);
+        signal(SIGTERM, SIG_IGN);
+        cout << "catching the signal" << sig << endl;
+    }
+    cout << "子进程退出" << endl;
+    // 编写善后代码（释放资源，提交或回滚事物）
+    exit(0);
 }
